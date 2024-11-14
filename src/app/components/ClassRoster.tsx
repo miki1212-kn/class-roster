5795cef08ce2925f1fbc59f8395c03a025af9b52
mk <kanzakimiki.12@gmail.com>
2024-11-14 12:13:14 +0900

    メインページ一旦作成

--- src/app/components/ClassRoster.tsx
+++ src/app/components/ClassRoster.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 type Student = {
   id: number;
   name: string;
+  studentNumber: number;
   position: string;
 };
 
@@ -20,6 +21,7 @@ const ClassRoster: React.FC<Props> = ({ students }) => {
         <div key={student.id}>
           <h3>{student.id}</h3>
           <h3>{student.name}</h3>
+          <p>学籍番号: {student.studentNumber}</p>
           <p>Position: {student.position}</p>
           <Link href={`/students/${student.id}`}>more</Link>
         </div>
