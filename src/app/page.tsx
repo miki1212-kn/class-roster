495908998538b2889a519615a958f88b87f31018
mk <kanzakimiki.12@gmail.com>
2024-11-23 16:04:39 +0900

    一旦リセット！

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,26 @@
 import React, { useEffect, useState } from "react";
 import ClassRoster from "./components/ClassRoster";
 // import Link from "next/link";
-// import styles from "./page.module.css";
+// import styles from "./styles.module.scss";
 
 const ClassRosterPage = () => {
-  const [students, setStudents] = useState([]);
+  //   const [students, setStudents] = useState([]);
 
-  useEffect(() => {
-    // apiフォルダのapiルートからデータを取得
-    fetch("/api/students")
-      .then((response) => response.json())
-      .then((data) => setStudents(data))
-      .catch((error) => console.error("Error fetching students:", error));
-  }, []);
+  //   useEffect(() => {
+  //     // apiフォルダのapiルートからデータを取得
+  //     fetch("/api/students")
+  //       .then((response) => response.json())
+  //       .then((data) => setStudents(data))
+  //       .catch((error) => console.error("Error fetching students:", error));
+  //   }, []);
 
   return (
     <>
       <h1>クラス名簿</h1>
-      <ClassRoster students={students} />
+      <ul>
+        <ClassRoster />
+      </ul>
+      {/* <ClassRoster students={students} /> */}
     </>
   );
 };
