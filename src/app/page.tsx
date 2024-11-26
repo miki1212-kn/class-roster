7c1709e80313df80fe4d7ae6fbc18039ec18c0d0
mk <kanzakimiki.12@gmail.com>
2024-11-26 15:16:08 +0900

    style調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { log } from "console";
 import React, { use, useEffect, useState } from "react";
 // import ClassRoster from "./components/ClassRoster";
 // import Link from "next/link";
-// import styles from "./styles.module.scss";
+import styles from "./styles.module.scss";
 
 const ClassRosterPage = () => {
   const [classList, setClassList] = useState<any[]>([]);
@@ -22,13 +21,16 @@ const ClassRosterPage = () => {
         if (!res.ok) {
           throw new Error(`HTTP error! Status: ${res.status}`);
         }
-        console.log(res);
+        // console.log(res);
         //resをjsonに変換
         const data = await res.json();
-        setClassList(data);
-        console.log("APIレスポンス:", data); //返ってきてた！
+        //dataの中に配列がある状態で返ってきてるので、classListには最初のdata[0]をセットする
+        setClassList(data[0]);
+        //配列で返ってきてる
+        console.log("APIレスポンス:", data);
       } catch (error) {
         console.error("エラー:", error);
+        setError("データの取得に失敗");
       } finally {
         //ローディング終了
         setLoading(false);
@@ -44,19 +46,37 @@ const ClassRosterPage = () => {
     return <p>{error}</p>;
   }
   return (
-    <>
+    <div className={styles.container}>
       <h1>クラス名簿</h1>
+
+      <div className={styles.headContainer}>
+        <form>
+          <select name="position" id="position">
+            <option value="designer">designer</option>
+            <option value="engineer">engineer</option>
+            <option value="directer">directer</option>
+          </select>
+          <input type="submit" value="絞り込む" />
+        </form>
+      </div>
       {/* classListのlengthが０より大きい時 */}
       {classList.length > 0 ? (
-        <ul>
+        <ul className={styles.classListContainer}>
           {classList.map((student, index) => (
-            <li key={index}>{student.name}</li>
+            <li key={index}>
+              <p className={styles.id}>{student.id}</p>
+              <p className={styles.name}>{student.name}</p>
+              <p className={styles.studentNumber}>{student.studentNumber}</p>
+              <p className={styles.position}>{student.position}</p>
+
+              <button>more</button>
+            </li>
           ))}
         </ul>
       ) : (
         <p>クラス名簿はありません。</p>
       )}
-    </>
+    </div>
   );
 };
 
